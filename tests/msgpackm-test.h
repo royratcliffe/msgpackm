3fb4b68e6349676cff7ead44493abe648c609a1f
Roy Ratcliffe <roy@ratcliffe.me>
2022-11-30 15:27:57 +0000

    feat: ASSERT macro for testing

---
+++ tests/msgpackm-test.h
@@ -0,0 +1,3 @@
+#include <stdlib.h>
+
+#define ASSERT(x) if (!(x)) do { exit(1); } while (0)
--- tests/swap16.c
+++ tests/swap16.c
@@ -1,11 +1,11 @@
 #include <msgpackm.h>
-#include <assert.h>
+#include "msgpackm-test.h"
 
 int swap16(int argc, char *argv[])
 {
     uint8_t incoming[] = {0x11, 0x22}, outgoing[2];
     msgpack_swap16(incoming, outgoing);
-    assert(outgoing[0] == 0x22);
-    assert(outgoing[1] == 0x11);
+    ASSERT(outgoing[0] == 0x22);
+    ASSERT(outgoing[1] == 0x11);
     return 0;
 }
