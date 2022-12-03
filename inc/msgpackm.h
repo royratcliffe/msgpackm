ff2d43c97be5817fc834f1d91a17ae8cccce92a4
Roy Ratcliffe <roy@ratcliffe.me>
2022-12-03 15:10:41 +0000

    docs: basic msgpack_swap16 documentation

--- inc/msgpackm.h
+++ inc/msgpackm.h
@@ -1,3 +1,7 @@
 #include <stdint.h>
 
+/*!
+ * \brief Swaps two octets.
+ * \details Performs a bit swap operation with indirection.
+ */
 void msgpack_swap16(const uint8_t *restrict swapping, uint8_t *restrict swapped);
