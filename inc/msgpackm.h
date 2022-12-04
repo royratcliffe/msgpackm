5a865f40518c0f2361eba5c64253161442f238d7
Roy Ratcliffe <roy@ratcliffe.me>
2022-12-04 12:56:33 +0000

    feat: msgpack_format_int

--- inc/msgpackm.h
+++ inc/msgpackm.h
@@ -4,6 +4,14 @@
 */
 #include <stdint.h>
 
+struct msgpack_formatter
+{
+    uint8_t *data;
+    size_t size;
+};
+
+int msgpack_format_int(struct msgpack_formatter *formatter, int value);
+
 /*!
  * \brief Swaps two octets.
  * \details Performs a bit swap operation with indirection.
