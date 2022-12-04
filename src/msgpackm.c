92c38b1d1795051feb3ff7ac8457a44e7b76f909
Roy Ratcliffe <roy@ratcliffe.me>
2022-12-04 13:38:46 +0000

    refactor: msgpack_format

--- src/msgpackm.c
+++ src/msgpackm.c
@@ -1,17 +1,22 @@
 #include <msgpackm.h>
 
+static int msgpack_format(struct msgpack_formatter *formatter, uint8_t xx)
+{
+    if (formatter->size < 1) return 0;
+    *formatter->data++ = xx;
+    formatter->size--;
+    return 1;
+}
+
 int msgpack_format_int(struct msgpack_formatter *formatter, int value)
 {
     if (-32 <= value && value <= 127)
     {
-        if (formatter->size < 1) return 0;
         /*
          * The following expression demotes the incoming integer from signed to
          * unsigned throwing away all more-significant bits beyond the eight.
          */
-        *formatter->data++ = value;
-        formatter->size--;
-        return 1;
+        return msgpack_format(formatter, value);
     }
     return 0;
 }
