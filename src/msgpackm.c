bc48fb47e2c7ee76f4e2e63dda2fd5d4eb9040a6
Roy Ratcliffe <roy@ratcliffe.me>
2022-12-04 14:50:36 +0000

    refactor: format

--- src/msgpackm.c
+++ src/msgpackm.c
@@ -1,10 +1,30 @@
 #include <msgpackm.h>
+#include <stdarg.h>
 
-static int msgpack_format(struct msgpack_formatter *formatter, uint8_t xx)
+/*!
+ * \brief Writes octets to formatter if space available.
+ *
+ * Expression \c va_arg(args, uint8_t) gives a warning about integer promotion,
+ * command-line option \c -Wvarargs on GCC.
+ *
+ * \param formatter Destination for span of octets.
+ * \param size Number of octets to write.
+ * \param ... Octets to write.
+ *
+ * \return Answers the number of octet \e spans completely formatted; zero or
+ * one. Returns zero for none written, or one for all written.
+ */
+inline static int format(struct msgpack_formatter *formatter, size_t size, ...)
 {
-    if (formatter->size < 1) return 0;
-    *formatter->data++ = xx;
-    formatter->size--;
+    if (formatter->size < size) return 0;
+    va_list args;
+    va_start(args, size);
+    while (size--)
+    {
+        *formatter->data++ = va_arg(args, int);
+        formatter->size--;
+    }
+    va_end(args);
     return 1;
 }
 
@@ -16,7 +36,11 @@ int msgpack_format_int(struct msgpack_formatter *formatter, int value)
          * The following expression demotes the incoming integer from signed to
          * unsigned throwing away all more-significant bits beyond the eight.
          */
-        return msgpack_format(formatter, value);
+        return format(formatter, 1, value);
+    }
+    if (-128 <= value && value <= 127)
+    {
+        return format(formatter, 2, 0xcc, value);
     }
     return 0;
 }
